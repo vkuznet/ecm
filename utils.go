c37ed4a05409da0eb1679b47e14c11b659ed011b
Valentin Kuznetsov <vkuznet@gmail.com>
2021-08-26 11:07:58 -0400

    Add backup function

--- utils.go
+++ utils.go
@@ -1,6 +1,12 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
+)
 
 // custom split function based on '---\n' delimiter
 func pwmSplitFunc(data []byte, atEOF bool) (advance int, token []byte, err error) {
@@ -19,4 +25,33 @@ func pwmSplitFunc(data []byte, atEOF bool) (advance int, token []byte, err error
 	}
 
 	return
+}
+
+// backup helper function to make a vault backup
+// based on https://github.com/mactsouk/opensource.com/blob/master/cp1.go
+func backup(src string) (int64, error) {
+	tstamp := time.Now().Format(time.RFC3339Nano)
+	dst := fmt.Sprintf("%s.backup-%s", src, tstamp)
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
 }
