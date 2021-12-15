2a0c6c2bad70f35232a172fffec152c3f189689b
Valentin Kuznetsov <vkuznet@gmail.com>
2021-12-15 13:10:21 -0500

    Add gpm logic

--- wasm/main.go
+++ wasm/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 	"syscall/js"
+
+	"github.com/vkuznet/gpm/crypt"
 )
 
 // main function sets JS "gpmDecode" function to call "decodeWrapper" Go counterpart
@@ -18,9 +22,25 @@ func decodeWrapper() js.Func {
 		if len(args) == 0 {
 			return wrap("", "Not enough arguments")
 		}
+		// here input should be vault file name, e.g.
+		// "/Users/vk/.gpm/Primary/acb8a9f7-6140-42d2-bb32-f730f7ab572f.aes"
 		input := args[0].String()
-		// perform decoding of the input
-		output := fmt.Sprintf("GPM: %s", input)
+		// perform decoding input file
+		passphrase := "test"
+		cipher := "aes"
+		arr := strings.Split(input, ".")
+		if len(arr) > 0 {
+			cipher = arr[1]
+		}
+		data, err := ioutil.ReadFile(input)
+		if err != nil {
+			wrap("", err.Error())
+		}
+		rec, err := crypt.Decrypt(data, passphrase, cipher)
+		if err != nil {
+			wrap("", err.Error())
+		}
+		output := fmt.Sprintf("GPM: %s, data=%s", input, string(rec))
 		return wrap(output, "")
 	})
 }
