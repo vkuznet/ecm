6244c4117e9286c5388820cde24d2c5994389d0c
Valentin Kuznetsov <vkuznet@gmail.com>
2022-01-11 15:59:18 -0500

    fix wasm file

--- static/js/go.js
+++ static/js/go.js
@@ -1,6 +1,5 @@
 const go = new Go();
-const wasmUrl = chrome.runtime.getURL("js/ecm.wasm");
-WebAssembly.instantiateStreaming(fetch(wasmUrl), go.importObject)
+WebAssembly.instantiateStreaming(fetch("static/js/ecm.wasm"), go.importObject)
     .then(result => {
     go.run(result.instance);
 });
