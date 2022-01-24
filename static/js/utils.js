bb93ab052d74573184d1734307b910d78bb07c10
Valentin Kuznetsov <vkuznet@gmail.com>
2022-01-24 17:43:56 -0500

    some clean-up

--- static/js/utils.js
+++ static/js/utils.js
@@ -23,14 +23,11 @@ function FlipTag(tag) {
 function readFile(input) {
     let file = input.files[0];
     let fileReader = new FileReader();
-    // TODO: replace readAsText to reading bytes
-//    fileReader.readAsText(file);
-//    fileReader.readAsBinaryString(file);
     fileReader.readAsArrayBuffer(file);
     fileReader.onload = function() {
         let data = buf2hex(fileReader.result);
-        console.log(file.name);
-        console.log(data);
+//        console.log(file.name);
+//        console.log(data);
         // call wasm uploadFile function
         uploadFile(file.name, file.size, file.type, data);
     };
@@ -38,6 +35,7 @@ function readFile(input) {
       alert(fileReader.error);
     };
 }
+// https://stackoverflow.com/questions/40031688/javascript-arraybuffer-to-hex
 function buf2hex(buffer) { // buffer is an ArrayBuffer
   return [...new Uint8Array(buffer)]
       .map(x => x.toString(16).padStart(2, '0'))
