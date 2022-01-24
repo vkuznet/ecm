d65adcfb5275178d2fad728aeff56d20c11165f9
Valentin Kuznetsov <vkuznet@gmail.com>
2022-01-24 15:24:39 -0500

    Add readFile function

--- static/js/utils.js
+++ static/js/utils.js
@@ -20,3 +20,19 @@ function FlipTag(tag) {
         }
     }
 }
+function readFile(input) {
+    let file = input.files[0];
+    let fileReader = new FileReader();
+    // TODO: replace readAsText to reading bytes
+    fileReader.readAsText(file);
+    fileReader.onload = function() {
+        let data = fileReader.result
+        console.log(file.name);
+        console.log(data);
+        // call wasm uploadFile function
+        uploadFile(file.name, file.size, file.type, data);
+    };
+    fileReader.onerror = function() {
+      alert(fileReader.error);
+    };
+}
