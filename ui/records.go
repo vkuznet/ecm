52a1505bf87f0af848a8035554dab99997cf9c4f
Valentin Kuznetsov <vkuznet@gmail.com>
2022-05-23 16:01:58 -0400

    update vault record

--- ui/records.go
+++ ui/records.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	fyne "fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
+	container "fyne.io/fyne/v2/container"
+	theme "fyne.io/fyne/v2/theme"
+	widget "fyne.io/fyne/v2/widget"
 	utils "github.com/vkuznet/ecm/utils"
 	vt "github.com/vkuznet/ecm/vault"
 )
@@ -32,7 +32,7 @@ func (a *vaultRecords) buildUI() *container.Scroll {
 	// list of entries
 	entries := widget.NewAccordion()
 	for _, rec := range _vault.Records {
-		entries.Append(widget.NewAccordionItem(rec.ID, a.recordContainer(rec.Map)))
+		entries.Append(widget.NewAccordionItem(rec.ID, a.recordContainer(rec)))
 	}
 
 	return container.NewScroll(container.NewVBox(
@@ -42,9 +42,12 @@ func (a *vaultRecords) buildUI() *container.Scroll {
 }
 
 // helper function to create record form item representation
-func (a *vaultRecords) formItem(key, val string) *widget.FormItem {
+// func (a *vaultRecords) formItem(key, val string) *widget.FormItem {
+func (a *vaultRecords) formItem(vrec vt.VaultRecord, key, val string) *widget.FormItem {
 	if key == "Password" || key == "password" {
 		rec := widget.NewPasswordEntry()
+		//         rec := widget.NewEntryWithData(val)
+		//         rec.Password = true
 		rec.Text = val
 		rec.Refresh()
 		recContainer := container.NewVBox(
@@ -54,32 +57,37 @@ func (a *vaultRecords) formItem(key, val string) *widget.FormItem {
 		)
 		return widget.NewFormItem(key, recContainer)
 	}
-	rec := &widget.Entry{Text: val, OnChanged: func(v string) {}}
+	rec := &widget.Entry{
+		Text: val,
+		OnChanged: func(v string) {
+			vrec.Map[key] = v
+		},
+	}
+	//     rec := widget.NewEntryWithData(val)
 	return widget.NewFormItem(key, rec)
 }
 
 // helper function to create record representation
-func (a *vaultRecords) recordContainer(record vt.Record) *fyne.Container {
+func (a *vaultRecords) recordContainer(record vt.VaultRecord) *fyne.Container {
 	// create entry object
-	orderedKeys := []string{
-		"Name", "Login", "Password", "URL", "Tags", "Note",
-	}
 	var items []*widget.FormItem
-	for _, k := range orderedKeys {
-		if v, ok := record[k]; ok {
-			items = append(items, a.formItem(k, v))
+	for _, k := range vt.OrderedKeys {
+		if v, ok := record.Map[k]; ok {
+			items = append(items, a.formItem(record, k, v))
 		}
 	}
 	// other keys can follow ordered ones
-	for k, v := range record {
-		if !utils.InList(k, orderedKeys) {
-			items = append(items, a.formItem(k, v))
+	for k, v := range record.Map {
+		if !utils.InList(k, vt.OrderedKeys) {
+			items = append(items, a.formItem(record, k, v))
 		}
 	}
 	form := &widget.Form{
 		Items:      items,
 		SubmitText: "Update",
-		OnSubmit:   func() { /* TODO: update entry record */ },
+		OnSubmit: func() {
+			record.WriteRecord(_vault.Directory, _vault.Secret, _vault.Cipher, _vault.Verbose)
+		},
 	}
 	recContainer := container.NewVBox(form)
 	return recContainer
