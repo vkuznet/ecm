20109eaa9e3fdde3142af5b0d775c7a2451ea5bd
Valentin Kuznetsov <vkuznet@gmail.com>
2022-05-24 14:30:26 -0400

    set preferences

--- ui/main.go
+++ ui/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
+	"os"
 
 	fyne "fyne.io/fyne/v2"
 	app "fyne.io/fyne/v2/app"
@@ -12,7 +14,7 @@ import (
 // various sizes of our application
 var windowSize, inputSize fyne.Size
 var fontColor color.Color
-var mobile bool
+var appKind string
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -21,21 +23,33 @@ func main() {
 	a.Settings().SetTheme(theme.DarkTheme())
 	w := a.NewWindow("ECM")
 
-	appSettings()
+	appSettings(a)
 	LoginWindow(a, w)
 
 	w.ShowAndRun()
 }
 
-// helper function to set application settings
-func appSettings() {
-	if mobile {
+// helper function to set application preferences/settings
+func appSettings(app fyne.App) {
+	// set some values for our app preferences
+	pref := app.Preferences()
+
+	// default values
+	cipher := "aes"
+	vdir := fmt.Sprintf("%s/.ecm/Primary", os.Getenv("HOME"))
+	fontSize := "Normal"
+	fontColor = color.White
+
+	// make changes depending on application kind
+	if appKind == "desktop" {
+		windowSize = fyne.NewSize(700, 600)
+		inputSize = fyne.NewSize(300, 50)
+	} else {
+		vdir = "/sdcard"
 		windowSize = fyne.NewSize(100, 400)
 		inputSize = fyne.NewSize(50, 50)
-		fontColor = color.White
-	} else {
-		windowSize = fyne.NewSize(700, 400)
-		inputSize = fyne.NewSize(300, 50)
-		fontColor = color.White
 	}
+	pref.SetString("VaultCipher", cipher)
+	pref.SetString("VaultDirectory", vdir)
+	pref.SetString("FontSize", fontSize)
 }
