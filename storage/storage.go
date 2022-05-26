607c049615927e58ff34f2cd5e19a7cb8d2a1c94
Valentin Kuznetsov <vkuznet@gmail.com>
2022-05-26 08:35:26 -0400

    Add new storage module

---
+++ storage/storage.go
@@ -0,0 +1,74 @@
+package storage
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Storage defines generic storage interface
+type Storage interface {
+	// Read reades from storage
+	Read(rid string) ([]byte, error)
+	// Write writes given record to storage using givne file name
+	Write(fname string, rec []byte) error
+	// Records return list of record ids from storage
+	Records() []string
+}
+
+// FileStorage provides file-system based storage
+type FileStorage struct {
+	Path string
+}
+
+// Read implements Storage.Read method
+func (f *FileStorage) Read(rid string) ([]byte, error) {
+	files, err := ioutil.ReadDir(f.Path)
+	if err != nil {
+		return []byte{}, err
+	}
+	var base, fname, ext string
+	for _, f := range files {
+		base = filepath.Base(f.Name())
+		ext = filepath.Ext(f.Name())
+		fname = strings.Replace(base, ext, "", -1)
+		if fname == rid {
+			break
+		}
+	}
+	fileName := filepath.Join(f.Path, base)
+	data, err := os.ReadFile(fileName)
+	return data, err
+
+}
+
+// Write implements Storage.Write method
+func (f *FileStorage) Write(fname string, rec []byte) error {
+	fileName := filepath.Join(f.Path, fname)
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Println("unable to create file name", fileName, " error ", err)
+		return err
+	}
+	defer file.Close()
+	_, err = file.Write(rec)
+	return err
+}
+
+// Records implement Storage Records method
+func (f *FileStorage) Records() ([]string, error) {
+	var records []string
+	files, err := ioutil.ReadDir(f.Path)
+	if err != nil {
+		return records, err
+	}
+	for _, f := range files {
+		base := filepath.Base(f.Name())
+		ext := filepath.Ext(f.Name())
+		fname := strings.Replace(base, ext, "", -1)
+		records = append(records, fname)
+	}
+	return records, nil
+}
