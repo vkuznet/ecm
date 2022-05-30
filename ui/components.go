b79f975c6b8beff6d7515084c4ce9f255723c620
Valentin Kuznetsov <vkuznet@gmail.com>
2022-05-30 12:59:54 -0400

    remove code which went to login.go

--- ui/components.go
+++ ui/components.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"image/color"
 	"log"
-	"os"
-	"time"
 
 	"fyne.io/fyne/v2"
 	container "fyne.io/fyne/v2/container"
-	layout "fyne.io/fyne/v2/layout"
-	theme "fyne.io/fyne/v2/theme"
 	widget "fyne.io/fyne/v2/widget"
 	vt "github.com/vkuznet/ecm/vault"
-	"golang.org/x/exp/errors"
 )
 
 // define our vault
@@ -31,115 +25,6 @@ func AppWindow(app fyne.App, w fyne.Window) {
 	w.SetMaster()
 	// custom theme
 	//     app.Settings().SetTheme(&grayTheme{})
-}
-
-func checkVault() {
-	if appKind == "desktop" {
-		if _, err := os.Stat(_vault.Directory); errors.Is(err, os.ErrNotExist) {
-			err := _vault.Create(_vault.Directory)
-			if err != nil {
-				log.Println("unable to create vault directory", _vault.Directory, "error: ", err)
-			}
-		}
-	}
-}
-
-// helper function to create appropriate login button with custom text and icon
-func loginButton(app fyne.App, w fyne.Window, entry *widget.Entry) *widget.Button {
-	return &widget.Button{
-		Text: "",
-		Icon: theme.LoginIcon(),
-		OnTapped: func() {
-			_vault.Secret = entry.Text
-			checkVault()
-			err := _vault.Read()
-			if err != nil {
-				ErrWindow(app, w, err)
-			} else {
-				AppWindow(app, w)
-			}
-		},
-	}
-}
-
-// LoginWindow represents login window
-func LoginWindow(app fyne.App, w fyne.Window) {
-	// custom theme
-	//     app.Settings().SetTheme(&grayTheme{})
-
-	// get vault records
-	if _vault == nil {
-		pref := app.Preferences()
-		cipher := pref.String("VaultCipher")
-		vdir := pref.String("VaultDirectory")
-		_vault = &vt.Vault{Directory: vdir, Cipher: cipher, Start: time.Now()}
-	}
-
-	password := widget.NewPasswordEntry()
-	password.OnSubmitted = func(p string) {
-		_vault.Secret = p
-		checkVault()
-		err := _vault.Read()
-		if err != nil {
-			ErrWindow(app, w, err)
-		} else {
-			AppWindow(app, w)
-		}
-	}
-	password.PlaceHolder = "Enter Master Password"
-	//     password.Resize(inputSize)
-
-	loginContainer := container.NewGridWrap(rowSize, password)
-
-	btnColor := color.NRGBA{0x79, 0x79, 0x79, 0xff}
-	btn := loginButton(app, w, password)
-	btnContainer := colorButtonContainer(btn, btnColor)
-	loginRowContainer := container.NewHBox(
-		loginContainer, btnContainer,
-	)
-	spacer := &layout.Spacer{}
-	contentContainer := container.NewVBox(
-		spacer,
-		loginRowContainer,
-		spacer,
-	)
-	content := container.NewCenter(contentContainer)
-
-	/*
-		label := ""
-		formItem := widget.NewFormItem(label, password)
-
-		form := &widget.Form{
-			Items: []*widget.FormItem{formItem},
-			OnSubmit: func() {
-				_vault.Secret = password.Text
-				checkVault()
-				err := _vault.Read()
-				if err != nil {
-					ErrWindow(app, w, err)
-				} else {
-					AppWindow(app, w)
-				}
-			},
-		}
-		text := &canvas.Text{Text: "Encrypted Content"}
-		text.Alignment = fyne.TextAlignCenter
-		spacer := &layout.Spacer{}
-
-		// set final container
-		content := container.NewVBox(
-			spacer,
-			text,
-			form,
-			spacer,
-		)
-	*/
-
-	// set window settings
-	w.SetContent(content)
-	w.Resize(windowSize)
-	w.Canvas().Focus(password)
-	w.SetMaster()
 }
 
 // Create will stitch together all ui components
