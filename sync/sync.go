9d595ac5120034c95104b20d8c581d0b0cdc69be
Valentin Kuznetsov <vkuznet@gmail.com>
2022-06-07 10:00:02 -0400

    Add EcmCreateConfig and EcmUpdateConfig functions

--- sync/sync.go
+++ sync/sync.go
@@ -1,7 +1,10 @@
 package sync
 
 import (
+	"bufio"
 	"context"
+	"fmt"
+	"os"
 
 	// load backend modules for rclone
 	_ "github.com/rclone/rclone/backend/dropbox"
@@ -11,17 +14,18 @@ import (
 
 	// rclone libraries
 	"github.com/rclone/rclone/cmd"
-	rconfig "github.com/rclone/rclone/fs/config"
+	"github.com/rclone/rclone/fs/config"
 	"github.com/rclone/rclone/fs/config/configfile"
 	"github.com/rclone/rclone/fs/operations"
+	"github.com/rclone/rclone/fs/rc"
 	"github.com/rclone/rclone/fs/sync"
 )
 
 // EcmSync provides a sync interface between source and destination
 // The code is based on https://rclone.org/ library
-func EcmSync(config, src, dst string) error {
-	if config != "" {
-		rconfig.SetConfigPath(config)
+func EcmSync(cpath, src, dst string) error {
+	if cpath != "" {
+		config.SetConfigPath(cpath)
 	}
 	configfile.Install()
 
@@ -32,4 +36,44 @@ func EcmSync(config, src, dst string) error {
 		return sync.Sync(context.Background(), fdst, fsrc, createEmptySrcDirs)
 	}
 	return operations.CopyFile(context.Background(), fdst, fsrc, srcFileName, srcFileName)
+}
+
+// EcmCreateConfig creates sync config to be used by ecm (and rclone)
+func EcmCreateConfig(cname string) error {
+	file, err := os.Create(cname)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	w := bufio.NewWriter(file)
+	w.Write([]byte("[local]\n"))
+	w.Write([]byte("type = local\n\n"))
+	w.Write([]byte("[dropbox]\n"))
+	w.Write([]byte("type = dropbox\n"))
+	w.Write([]byte("env_auth = true\n\n"))
+	w.Write([]byte("[pcloud]\n"))
+	w.Write([]byte("type = pcloud\n"))
+	w.Write([]byte("hostname = api.pcloud.com\n"))
+	w.Flush()
+	return nil
+}
+
+// EcmUpdateConfig update given config file and add appropriate tokens to cloud providers
+func EcmUpdateConfig(cpath, provider string) error {
+	fmt.Println("EcmUpdateCondig: ", cpath)
+	if cpath != "" {
+		config.SetConfigPath(cpath)
+	}
+	configfile.Install()
+
+	// add tokens to cloud providers
+	in := rc.Params{}
+	in["env_auth"] = true
+	in["config_refresh_token"] = true
+	opts := config.UpdateRemoteOpt{}
+	_, err := config.UpdateRemote(context.Background(), provider, in, opts)
+	if err != nil {
+		return err
+	}
+	return nil
 }
